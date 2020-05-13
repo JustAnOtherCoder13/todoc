3610c3a3f66b16bcfcf2513279be5c9db193f350
JustAnOtherCoder13 <justanothercoder13@gmail.com>
2020-05-13 14:18:06 +0200

    ok resolve with a generator

--- app/src/test/java/com/cleanup/todoc/TaskUnitTest.java
+++ app/src/test/java/com/cleanup/todoc/TaskUnitTest.java
@@ -11,34 +11,38 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
+import static com.cleanup.todoc.ProjectStaticList.generateProjects;
 import static com.cleanup.todoc.ui.MainActivity.allProjects;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 
 public class TaskUnitTest {
     private List<Project> projects;
-    private final static Project PROJECT1 = new Project(1L,"Projet Tartampion",0xFFEADAD1);
-    private final static Project PROJECT2 = new Project(2L,"Projet Lucidia",0xFFB4CDBA);
-    private final static Project PROJECT3 = new Project(3L,"Projet Circus",0xFFA3CED2);
+    private long projectId1;
+    private long projectId2;
+    private long projectId3;
 
     @Before
-    public void initDb(){
-        projects.add(PROJECT1);
-        projects.add(PROJECT2);
-        projects.add(PROJECT3);
+    public void initDb(){projects = generateProjects();
+    projectId1 = projects.get(0).getId();
+    projectId2 = projects.get(1).getId();
+    projectId3 = projects.get(2).getId();
     }
     @Test
     public void test_projects() {
-        final Task task1 = new Task( projects.get(0).getId(), "task 1", new Date().getTime());
-        final Task task2 = new Task( projects.get(1).getId(), "task 2", new Date().getTime());
-        final Task task3 = new Task( projects.get(2).getId(), "task 3", new Date().getTime());
+        final Task task1 = new Task( projectId1, "task 1", new Date().getTime());
+        final Task task2 = new Task( projectId2, "task 2", new Date().getTime());
+        final Task task3 = new Task( projectId3, "task 3", new Date().getTime());
         final Task task4 = new Task( 4, "task 4", new Date().getTime());
 
-        assertEquals("Projet Tartampion", task1.getProject().getName());
-        assertEquals("Projet Lucidia", task2.getProject().getName());
-        assertEquals("Projet Circus", task3.getProject().getName());
-        assertNull(task4.getProject());
+        assertEquals(projectId1, task1.getProjectId());
+        assertEquals(projectId2, task2.getProjectId());
+        assertEquals(projectId3, task3.getProjectId());
+        assertFalse(task4.getProjectId()==projectId3||task4.getProjectId()==projectId2||
+                            task4.getProjectId()==projectId1);
+        //assertNull(task4.getProject());
     }
 
     @Test
